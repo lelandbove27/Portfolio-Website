4ebcd714be694aaebbe8aa3314a06cf13decc571
lbove27 <lbove@ucsd.edu>
2022-08-13 21:49:48 -0700

    added movie finder, discord bot, and physics project content

--- projects.js
+++ projects.js
@@ -1,7 +1,17 @@
-let dbseImageSource = document.getElementById('img1').src;
-let dbseDisplayedImage = document.getElementById('dbse-displayed-image');
-dbseDisplayedImage.src = dbseImageSource;
+let rmImageSource = document.getElementById('img1').src;
+let rmDisplayedImage = document.getElementById('rm-displayed-image');
+rmDisplayedImage.src = rmImageSource;
 
-function displayImage() {
+//query select all displayed-image elements, add the
+//default class to all of the first images in each 
+//thing and then query select all of those and then
+//match them up in the array properly for all default images
 
-}
+let smallImages = document.querySelectorAll('.small-image');
+smallImages.forEach(image => {
+    image.addEventListener('click', function(event) {
+        let destination = event.target.parentNode;
+        destination = destination.previousElementSibling;
+        destination.src = image.src;
+    });
+});
