f37b8ae2326de8cb5980faf07a2a4e42a3afcd82
lbove27 <lbove@ucsd.edu>
2023-09-18 18:54:38 -0700

    big update

--- projects.js
+++ projects.js
@@ -10,6 +10,14 @@ let mfImageSource = document.getElementById('mf-img1').src;
 let mfDisplayedImage = document.getElementById('mf-displayed-image');
 mfDisplayedImage.src = mfImageSource;
 
+let daImageSource = document.getElementById('da-img1').src;
+let daDisplayedImage = document.getElementById('da-displayed-image');
+daDisplayedImage.src = daImageSource;
+
+let sdbImageSource = document.getElementById('sdb-img1').src;
+let sdbDisplayedImage = document.getElementById('sdb-displayed-image');
+sdbDisplayedImage.src = sdbImageSource;
+
 let smallImages = document.querySelectorAll('.small-image');
 smallImages.forEach(image => {
     image.addEventListener('click', function(event) {
